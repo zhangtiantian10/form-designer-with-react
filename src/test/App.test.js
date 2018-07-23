618492c899fc9dc76b99b52b0566ef2f4c9c6618
Tian Zhang <tizhang@CNtizhang.local>
2018-07-23 22:18:41 +0800

    write App component test
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Adapter from 'enzyme-adapter-react-16';
 import App from '../component/App';
+import {shallow, configure} from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+configure({ adapter: new Adapter() });
+
+describe('App', () => {
+  it('should display edit button when click preview button', function () {
+    const app = shallow(<App/>);
+
+    expect(app.find('button#changeButton').text()).toEqual('Preview')
+    expect(app.state().isPreview).toEqual(false);
+
+    app.find('button#changeButton').simulate('click');
+
+    expect(app.find('button#changeButton').text()).toEqual('Edit')
+    expect(app.state().isPreview).toEqual(true);
+  });
+
+  it('should list add text when select text input in modal click add button', function () {
+    const app = shallow(<App/>);
+    app.find('button.add-button').simulate('click');
+    app.find('#textInput').simulate('click', {target: {value: 'text'}});
+    app.find('button#addButton').simulate('click');
+
+    expect(app.state('list')).toEqual(['text'])
+
+    app.find('#datePicker').simulate('click', {target: {value: 'date'}});
+    app.find('button#addButton').simulate('click');
+
+    expect(app.state('list')).toEqual(['text', 'date'])
+
+  });
+})
